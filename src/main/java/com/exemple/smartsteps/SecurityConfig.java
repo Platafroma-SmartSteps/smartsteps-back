011a7ccaf4d5817cb0451f22078c3e28ed9780d8
Felipe Santos Damasceno <lipisantoss@hotmail.com>
2023-12-17 01:23:48 -0300

    continuação do cadastro

---
+++ src/main/java/com/exemple/smartsteps/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.exemple.smartsteps;
+
+import com.exemple.smartsteps.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws  Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DefaultSecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+       httpSecurity.csrf().disable().authorizeHttpRequests().requestMatchers("/estudante/registrar").permitAll().anyRequest().authenticated().and().authenticationManager(authenticationManager).httpBasic();
+        return httpSecurity.build();
+    }
+
+}
---
+++ src/main/java/com/exemple/smartsteps/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.exemple.smartsteps.services;
+
+import com.exemple.smartsteps.models.Estudante;
+import com.exemple.smartsteps.repositories.EstudanteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private EstudanteRepository estudanteRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Estudante estudante = estudanteRepository.findEstudanteByEmailEstudante(email);
+
+        if(estudante == null) {
+            throw  new UsernameNotFoundException("Usuário não existe");
+        }
+
+        return  User.builder().username(estudante.getEmailEstudante()).password(estudante.getSenhaEstudante()).roles("STUDENT").build();
+    }
+
+}
